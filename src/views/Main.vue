c802f413639045a07c7da7000d8de4df9b2f4105
cancan <1048042379@qq.com>
2018-04-20 22:08:52 +0800

    调整整体布局方式

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,13 @@
 <template>
   <el-container>
-    <el-header class="my-header-nav">
+    <el-header class="nav-bar-header">
       <nav-header/>
     </el-header>
-    <el-container style="margin-top: 60px">
-      <el-aside style="width: 250px">
+    <el-container class="nav-bar-container">
+      <el-aside class="nav-bar-aside">
         <nav-bar/>
       </el-aside>
-      <el-main>
+      <el-main class="nav-bar-main">
         <router-view/>
       </el-main>
     </el-container>
@@ -34,11 +34,31 @@
 </script>
 
 <style scoped>
-  .my-header-nav {
+  .nav-bar-header {
     padding: 0;
     position: fixed;
     width: 100%;
     height: 60px;
     z-index: 1
+  }
+
+  .nav-bar-main {
+    height: 100%;
+    overflow: auto;
+  }
+
+  .nav-bar-container {
+    margin-top: 60px;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    padding-bottom: 50px
+  }
+
+  .nav-bar-aside {
+    width: 250px;
+    height: 100%;
+    overflow: auto;
+    padding-bottom: 30px
   }
 </style>
